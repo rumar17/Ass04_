bf40f9cfd5e1377338f16226f36cfb5dea8fdb90
june-dt <juneee.dt@gmail.com>
2023-11-21 14:01:46 +0800

    Added Components

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,15 +30,60 @@
         {
             this.components = new System.ComponentModel.Container();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.gbxManual = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.gbxConfig = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // gbxManual
+            // 
+            this.gbxManual.Location = new System.Drawing.Point(12, 73);
+            this.gbxManual.Name = "gbxManual";
+            this.gbxManual.Size = new System.Drawing.Size(150, 250);
+            this.gbxManual.TabIndex = 0;
+            this.gbxManual.TabStop = false;
+            this.gbxManual.Text = "Manual Control";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(168, 185);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 138);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Auto";
+            // 
+            // gbxConfig
+            // 
+            this.gbxConfig.Location = new System.Drawing.Point(12, 12);
+            this.gbxConfig.Name = "gbxConfig";
+            this.gbxConfig.Size = new System.Drawing.Size(355, 55);
+            this.gbxConfig.TabIndex = 2;
+            this.gbxConfig.TabStop = false;
+            this.gbxConfig.Text = "Configuration";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(168, 80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(199, 99);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(379, 346);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.gbxConfig);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gbxManual);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Fan Control";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -46,6 +91,10 @@
         #endregion
 
         private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.GroupBox gbxManual;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox gbxConfig;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
