b11b85a46df6db0acb0f0a23df18833edf476d2c
Marc Jhun Dela Torre Tampus <151604191+june-dt@users.noreply.github.com>
2023-12-06 05:24:38 +0800

    Add files via upload
    for final checking

--- ass04_TampusTicod.ino
+++ ass04_TampusTicod.ino
@@ -2,7 +2,7 @@ int speed;
 byte temp;
 int longTemp;
 
-int timeDelay = 1000;
+int timeDelay = 200;
 byte sensorValue;
 long currTime;
 
@@ -22,24 +22,10 @@ void loop() {
     
     if (Serial.read() == '\n') {
       // If a newline character is received, indicating the end of the integer
+
       analogWrite(3, speed);
     }
-
-    /*if (millis() - currTime == timeDelay){
-
-      currTime = millis();
-
-      int sensorValue = analogRead(A0);
-
-      Serial.println(sensorValue);
-    }*/
-
-    
-
   }
-
-
-
 
   if (millis() - currTime >= timeDelay){
 
@@ -51,19 +37,4 @@ void loop() {
 
     Serial.println(sensorValue);
   }
-
-
-  // Read the analog value from LM35 sensor
-  //int sensorValue = analogRead(A0);
-
-  // Convert the analog value to temperature in Celsius
-  /*float temperature = (sensorValue * 0.00488) * 100.0;
-
-  // Display temperature on Serial Monitor
-  Serial.print(sensorValue);
-  Serial.print(" | ");
-  Serial.print("Temperature: ");
-  Serial.print(temperature);
-  Serial.println(" °C");*/
-
 }
